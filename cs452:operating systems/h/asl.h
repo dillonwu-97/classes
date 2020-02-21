2e92cd2c3cac158889bd19772708e7d2a8a92d5b
Dillon Wu <dillonwu999@gmail.com>
2020-02-20 20:13:48 -0500

    queues and headers
    Former-commit-id: bfa18ff13cff0c111698afb59d43a6a8200d14ed

---
+++ cs452:operating systems/h/asl.h
@@ -0,0 +1,10 @@
+/* THIS CODE IS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING CODE
+WRITTEN BY OTHER STUDENTS.
+Dillon Wu */
+// semaphore descriptor type
+typedef struct semd_t {
+	struct semd_t *s_next, // next element on the queue
+			*s_prev; // previous element on the queue
+	int *s_semAdd; // pointer to the semaphore proper
+	proc_link s_link; // pointer/index to the tail of the queue of processes blocked on this semaphore
+} semd_t;
---
+++ cs452:operating systems/h/procq.e
@@ -0,0 +1,13 @@
+/* THIS CODE IS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING CODE
+WRITTEN BY OTHER STUDENTS.
+Dillon Wu */
+
+#include "procq.h"
+extern void insertProc();
+extern proc_t* removeProc();
+extern proc_t* outProc();
+extern proc_t* allocProc();
+extern void freeProc();
+extern proc_t* headQueue();
+extern void initProc();
+ 
