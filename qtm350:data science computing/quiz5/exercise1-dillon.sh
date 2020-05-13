35162633cf730b36ff43a31265836d83ac8579c7
Dillon Wu <dillonwu999@gmail.com>
2020-05-13 18:16:23 -0400

    deleted a class
    Former-commit-id: f94f7c0e09e1bda7d496115b67e2829000403d39

--- qtm350:data science computing/quiz5/exercise1-dillon.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-# This program prints the number of unique permissions in a directory
-# This program is for Linux (stat command doesn't exist on Mac)
-# Usage: ./exercise-dillon $directory
-
-#!/bin/bash
-echo $(( $(ls -l $1 | cut -d ' ' -f1 | sort | uniq | wc -l)-1 ))
